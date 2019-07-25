e193754151aa2aa428a30b44d59f2250f1115da6
zhangqiaowenxiang <zhangqiaowenxiang@0easy.com>
2019-07-25 09:43:44 +0800

    test new submitter

--- app/src/main/java/com/oeasy/apt/MainActivity.java
+++ app/src/main/java/com/oeasy/apt/MainActivity.java
@@ -2,7 +2,9 @@ package com.oeasy.apt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.oeasy.common.Hello;
 import com.oeasy.libannotation.OutputModulePackage;
 
 @OutputModulePackage(module = "common", packagePath = "com/oeasy/common")
@@ -12,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Log.e("zqwx", " - "+ Hello.keyAZqwx);
+        Log.e("zqwx", " - "+ Hello.LIB_ALPHA_VALUEAMAO);
 
     }
 }
